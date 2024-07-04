6cb796db1cf25cbe74d98ce8a564fc5a7a6af8cd
permaficus <abukhalif2019@gmail.com>
2024-07-04 20:36:13 +0700

    chore(): add auth token

--- src/worker/query.ts
+++ src/worker/query.ts
@@ -1,6 +1,8 @@
-import { hashedPassword, isPasswordMatch } from "@/libs/bcrypt";
+import { isPasswordMatch } from "@/libs/bcrypt";
 import { getAllConversations, getAllMessages, getProfileById, getUserPassword } from "@/model";
 import { QueryArguments } from "@/type/types";
+import { jwt } from '@/libs/webtoken';
+import Crypto from 'crypto';
 
 export const fetchUserProfile = async (args: QueryArguments): Promise<object | undefined> => {
     return await getProfileById({
@@ -21,12 +23,15 @@ const getCurrentPassword = async (username: string): Promise<string> => {
     const { password } = await getUserPassword(username);
     return password
 }
-export const userLogin = async (_: undefined, args: { body: { username: string, password: string }}, context: object): Promise<string> => {
+export const userLogin = async (_: undefined, args: { body: { username: string, password: string }}, context: object): Promise<any> => {
     const currentPass = await getCurrentPassword(args.body.username);
     const passwordMatched = await isPasswordMatch(args.body.password, currentPass)
     if (passwordMatched) {
         // create token
-        return `Success`
+        return {
+            status: `Success`,
+            token: jwt.createAuthToken({ user: args.body.username, iss: 'yuvee-chat' }, { jwtid: Crypto.randomUUID() })
+        }
     } else {
         throw new Error(`Login Failed: Username or Password didn't match`)
     }
